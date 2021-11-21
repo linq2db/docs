dd08e927244e7e162080b09970dbb2b6793c4d1f
MaceWindu <MaceWindu@users.noreply.github.com>
2021-11-21 14:10:01 +0100

    simplify repo structure, remove nuget.exe

--- local.cmd
+++ local.cmd
@@ -1,2 +1,10 @@
 CLS
+
+WHERE nuget.exe >nul 2>&1
+IF %errorlevel% NEQ 0 (
+ECHO Cannot find nuget.exe. Add it to PATH or place it to current folder
+ECHO nuget.exe could be downloaded from https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+GOTO :EOF
+)
+
 powershell build/build.ps1 0
