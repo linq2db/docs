764473a04d27d9bad74b39ed0f76d434f15cd9ab
Mariana Hladka <mhladk@softserveinc.com>
2023-02-09 16:44:24 +0200

    Added new sections.

--- local.cmd
+++ local.cmd
@@ -7,4 +7,4 @@ ECHO Cannot find nuget.exe. Add it to PATH or place it to current folder
 ECHO nuget.exe could be downloaded from https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 GOTO :EOF
 )
-powershell -Command .\build.ps1 -deploy $false
+pwsh.exe -Command .\build.ps1 -deploy $false
