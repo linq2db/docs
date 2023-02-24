41a7de1a58c5d82d4f212ecd61bef65dffd041ee
MaceWindu <MaceWindu@users.noreply.github.com>
2023-02-24 15:25:19 +0100

    docfx dotnet tool migration (#42)
    * initial work on docfx tool migration
    
    * pin tool runtime to 6.0 version
    
    * remove IdentityServer4 project
    
    * install net45 reference assemblies
    
    * fix net45 install
    
    * try older image
    
    * - fix identity submodule branch
    
    - add explicit restore as docfx doesn't so it
    
    * add .net 7 sdk
    
    * sho te nado...

--- build.ps1
+++ build.ps1
@@ -8,15 +8,21 @@ Write-Host Cleanup previous build artifacts...
 if ([System.IO.Directory]::Exists('_site')) { Remove-Item _site -Recurse -Force }
 if ([System.IO.Directory]::Exists('source/api')) { Remove-Item source/api -Recurse -Force }
 if ([System.IO.Directory]::Exists('linq2db.github.io')) { Remove-Item linq2db.github.io -Recurse -Force }
-Write-Host Done
 
 Write-Host Prepare tooling...
-&"nuget.exe" install msdn.4.5.2 -ExcludeVersion -OutputDirectory packages -Prerelease
-&"nuget.exe" install docfx.console -ExcludeVersion -OutputDirectory packages
-Write-Host Done
+dotnet tool install docfx -g --framework net6.0
+
+Write-Host Restore...
+dotnet restore 'submodules/linq2db/Source/LinqToDB/LinqToDB.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.AspNet/LinqToDB.AspNet.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.Remote.Grpc/LinqToDB.Remote.Grpc.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.Remote.Wcf/LinqToDB.Remote.Wcf.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.Tools/LinqToDB.Tools.csproj'
+dotnet restore 'submodules/linq2db.EntityFrameworkCore/Source/LinqToDB.EntityFrameworkCore/linq2db.EntityFrameworkCore.csproj'
+dotnet restore 'submodules/LinqToDB.Identity/src/LinqToDB.Identity/LinqToDB.Identity.csproj'
 
 Write-Host Build DocFX documentation...
-packages/docfx.console/tools/docfx.exe source/docfx.json -f
+docfx source/docfx.json
 if ($LASTEXITCODE -ne 0)
 {
     throw "DocFx build failed";
--- local.cmd
+++ local.cmd
@@ -1,10 +1,4 @@
 ECHO OFF
 CLS
 
-WHERE nuget.exe >nul 2>&1
-IF %errorlevel% NEQ 0 (
-ECHO Cannot find nuget.exe. Add it to PATH or place it to current folder
-ECHO nuget.exe could be downloaded from https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-GOTO :EOF
-)
 powershell -Command .\build.ps1 -deploy $false
