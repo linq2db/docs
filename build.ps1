195e5093db8828e0931e73e6a9c0ed24396ef275
MaceWindu <MaceWindu@users.noreply.github.com>
2023-02-24 14:48:11 +0100

    - fix identity submodule branch
    - add explicit restore as docfx doesn't so it

--- build.ps1
+++ build.ps1
@@ -8,11 +8,18 @@ Write-Host Cleanup previous build artifacts...
 if ([System.IO.Directory]::Exists('_site')) { Remove-Item _site -Recurse -Force }
 if ([System.IO.Directory]::Exists('source/api')) { Remove-Item source/api -Recurse -Force }
 if ([System.IO.Directory]::Exists('linq2db.github.io')) { Remove-Item linq2db.github.io -Recurse -Force }
-Write-Host Done
 
 Write-Host Prepare tooling...
 dotnet tool install docfx -g --framework net6.0
-Write-Host Done
+
+Write-Host Restore...
+dotnet restore 'submodules/linq2db/Source/LinqToDB/LinqToDB.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.AspNet/LinqToDB.AspNet.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.Remote.Grpc/LinqToDB.Remote.Grpc.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.Remote.Wcf/LinqToDB.Remote.Wcf.csproj'
+dotnet restore 'submodules/linq2db/Source/LinqToDB.Tools/LinqToDB.Tools.csproj'
+dotnet restore 'submodules/linq2db.EntityFrameworkCore/Source/LinqToDB.EntityFrameworkCore/linq2db.EntityFrameworkCore.csproj'
+dotnet restore 'submodules/LinqToDB.Identity/src/LinqToDB.Identity/LinqToDB.Identity.csproj'
 
 Write-Host Build DocFX documentation...
 docfx source/docfx.json
