7bc96e827ad695742561db8954d7e3e1d2b84c9a
MaceWindu <MaceWindu@users.noreply.github.com>
2023-05-18 15:46:07 +0200

    modern template tuning

---
+++ source/templates/custom/public/main.js
@@ -0,0 +1,15 @@
+export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/linq2db/linq2db',
+            title: 'Linq To DB'
+        },
+        {
+            icon: 'twitter',
+            href: 'https://twitter.com/linq2db',
+            title: 'Releases Twitter Feed'
+        }
+    ]
+}
+
