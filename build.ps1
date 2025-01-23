230310ff1e59cf429c03d824936c7223c4054ff2
MaceWindu <MaceWindu@users.noreply.github.com>
2025-01-23 11:58:27 +0100

    more fixes

--- build.ps1
+++ build.ps1
@@ -13,16 +13,13 @@ Write-Host Prepare tooling...
 dotnet tool install docfx -g
 
 Write-Host Restore...
-dotnet restore 'submodules/linq2db/Source/LinqToDB/LinqToDB.csproj'
-dotnet restore 'submodules/linq2db/Source/LinqToDB.AspNet/LinqToDB.AspNet.csproj'
-dotnet restore 'submodules/linq2db/Source/LinqToDB.Remote.Grpc/LinqToDB.Remote.Grpc.csproj'
-dotnet restore 'submodules/linq2db/Source/LinqToDB.Remote.Wcf/LinqToDB.Remote.Wcf.csproj'
-dotnet restore 'submodules/linq2db/Source/LinqToDB.Tools/LinqToDB.Tools.csproj'
-dotnet restore 'submodules/linq2db.EntityFrameworkCore/Source/LinqToDB.EntityFrameworkCore/linq2db.EntityFrameworkCore.csproj'
-dotnet restore 'submodules/LinqToDB.Identity/src/LinqToDB.Identity/LinqToDB.Identity.csproj'
+# workaround for https://github.com/dotnet/docfx/pull/8375
+# also works as workaround for https://github.com/dotnet/docfx/issues/9775
+dotnet build -c Release 'submodules/linq2db/Source/LinqToDB.FSharp/LinqToDB.FSharp.fsproj'
 
 Write-Host Build DocFX documentation...
 docfx source/docfx.json
+
 if ($LASTEXITCODE -ne 0)
 {
     throw "DocFx build failed";
